447bb953f4f234262c461006069a998b891c04aa
SHHam12 <hamsukhwa@gmail.com>
2017-12-30 18:25:48 -0500

    delete image

--- sukhwagram/images/views.py
+++ sukhwagram/images/views.py
@@ -189,6 +189,14 @@ class ModerateComments(APIView):
 
 class ImageDetail(APIView):
 
+    def find_own_image(self, image_id, user):
+
+        try:
+            image = models.Image.objects.get(id = image_id, creator = user)
+            return image
+        except models.Image.DoesNotExist:
+            return None
+
     def get(self, request, image_id, format = None):
 
         user = request.user
@@ -206,13 +214,14 @@ class ImageDetail(APIView):
 
         user = request.user
 
-        try:
-            image = models.Image.objects.get(id = image_id, creator = user)
-        except models.Image.DoesNotExist:
-            return Response(status = status.HTTP_401_UNAUTHORIZED)
+        image = self.find_own_image(image_id, user)
+
+        if image is None: 
+
+            return Resonse(status = status.HTTP_401_UNAUTHORIZED)
 
         serializer = serializers.InputImageSerializer(
-            image, data = request.data, partial = True)
+             image, data = request.data, partial = True)
 
         if serializer.is_valid():
 
@@ -223,3 +232,19 @@ class ImageDetail(APIView):
         else:
 
             return Response(data = serializer.errors, status = status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, image_id, format = None):
+
+        user = request.user
+
+        image = self.find_own_image(image_id, user)
+
+        if image is None: 
+
+            return Response(status = status.HTTP_401_UNAUTHORIZED)
+
+        image.delete()
+
+        return Response(status = status.HTTP_204_NO_CONTENT)
+
+        
