e02994bc33fbc98d9475cf46adf4122f66875d76
SHHam12 <hamsukhwa@gmail.com>
2018-01-15 02:54:16 -0500

    Closing Likes

--- frontend/src/components/FeedPhoto/presenter.js
+++ frontend/src/components/FeedPhoto/presenter.js
@@ -27,6 +27,7 @@ const FeedPhoto = (props, context) => {
           number={props.like_count}
           isLiked={props.is_liked}
           photoId={props.id}
+          openLikes={props.openLikes}
         />
         <PhotoComments
           caption={props.caption}
@@ -36,7 +37,9 @@ const FeedPhoto = (props, context) => {
         <TimeStamp time={props.natural_time} />
         <CommentBox photoId={props.id} />
       </div>
-      {props.seeingLikes && <UserList title={context.t("Likes")} />}
+      {props.seeingLikes && (
+        <UserList title={context.t("Likes")} closeLikes={props.closeLikes} />
+      )}
     </div>
   );
 };
@@ -66,7 +69,10 @@ FeedPhoto.propTypes = {
     })
   ).isRequired,
   natural_time: PropTypes.string.isRequired,
-  is_liked: PropTypes.bool.isRequired
+  is_liked: PropTypes.bool.isRequired,
+  seeingLikes: PropTypes.bool.isRequired,
+  openLikes: PropTypes.func.isRequired,
+  closeLikes: PropTypes.func.isRequired
 };
 
 export default FeedPhoto;
