bea04452b8ce14c9a29242df8af1ab00bc681f86
SHHam12 <hamsukhwa@gmail.com>
2018-01-15 08:55:54 -0500

    Explore

--- frontend/src/redux/modules/user.js
+++ frontend/src/redux/modules/user.js
@@ -7,6 +7,7 @@ const LOGOUT = "LOGOUT";
 const SET_USER_LIST = "SET_USER_LIST";
 const FOLLOW_USER = "FOLLOW_USER";
 const UNFOLLOW_USER = "UNFOLLOW_USER";
+const SET_EXPLORE = "SET_EXPLORE";
 
 // action creators
 
@@ -41,6 +42,13 @@ function setUnfollowUser(userId) {
   return {
     type: UNFOLLOW_USER,
     userId
+  };
+}
+
+function setExplore(userList) {
+  return {
+    type: SET_EXPLORE,
+    userList
   };
 }
 
@@ -174,6 +182,25 @@ function unfollowUser(userId) {
   };
 }
 
+function getExplore() {
+  return (dispatch, getState) => {
+    const { user: { token } } = getState();
+    fetch("/users/explore/", {
+      headers: {
+        Authorization: `JWT ${token}`,
+        "Content-Type": "application/json"
+      }
+    })
+      .then(response => {
+        if (response.status === 401) {
+          dispatch(logout());
+        }
+        return response.json();
+      })
+      .then(json => dispatch(setExplore(json)));
+  };
+}
+
 // initial state
 
 const initialState = {
@@ -195,6 +222,8 @@ function reducer(state = initialState, action) {
       return applyFollowUser(state, action);
     case UNFOLLOW_USER:
       return applyUnfollowUser(state, action);
+    case SET_EXPLORE:
+      return applySetExplore(state, action);
     default:
       return state;
   }
@@ -251,6 +280,15 @@ function applyUnfollowUser(state, action) {
   return { ...state, userList: updatedUserList };
 }
 
+function applySetExplore(state, action) {
+  const { userList } = action;
+  return {
+    ...state,
+    userList
+  };
+}
+
+
 // exports
 
 const actionCreators = {
@@ -260,7 +298,8 @@ const actionCreators = {
   logout,
   getPhotoLikes,
   followUser,
-  unfollowUser
+  unfollowUser,
+  getExplore
 };
 
 export { actionCreators };
